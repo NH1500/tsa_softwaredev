981dad675b92e080c98a79f8f83effef9c620b32
Nathan Hryniewicz <nhrynicom@gmail.com>
2023-06-03 20:46:20 -0400

    added to gitignore

--- tsa_softwaredev/lib/constants/api_consts.dart
+++ tsa_softwaredev/lib/constants/api_consts.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
 const String BASE_URL = "https://api.openai.com/v1/";
-const String API_KEY = "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";
+const String API_KEY = "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";
--- tsa_softwaredev/lib/services/api_service.dart
+++ tsa_softwaredev/lib/services/api_service.dart
@@ -54,39 +54,4 @@ class ApiService {
       rethrow;
     }
   }
-
-  // Send Message fct
-  static Future<List<ChatModel>> sendMessage(
-      {required String message, required String modelId}) async {
-    try {
-      log("modelId $modelId");
-      var response = await http.post(
-        Uri.parse("$BASE_URL/completions"),
-        headers: {
-          'Authorization': 'Bearer $API_KEY',
-          "Content-Type": "application/json"
-        },
-        body: jsonEncode(
-          {
-            "model": modelId,
-            "prompt": message,
-            "max_tokens": 300,
-          },
-        ),
-      );
-
-      // Map jsonResponse = jsonDecode(response.body);
-
-      Map jsonResponse = json.decode(utf8.decode(response.bodyBytes));
-      if (jsonResponse['error'] != null) {
-        // print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
-        throw HttpException(jsonResponse['error']["message"]);
-      }
-      List<ChatModel> chatList = [];
-      return chatList;
-    } catch (error) {
-      log("error $error");
-      rethrow;
-    }
-  }
 }
