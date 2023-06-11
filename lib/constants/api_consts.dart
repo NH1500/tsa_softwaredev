eb9c723a76bd893c0828c39df4becc1b93d992ac
Nathan Hryniewicz <nhrynicom@gmail.com>
2023-06-10 20:21:37 -0400

    Update api_consts.dart

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
 const String BASE_URL = "https://api.openai.com/v1/";
-//const String API_KEY = String.fromEnvironment("API_KEY");
-const String API_KEY = "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";
+const String API_KEY = String.fromEnvironment("API_KEY");
